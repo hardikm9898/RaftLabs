a7b6a3a2321a28720e84d09d6d196d122f0f0c99
hardik <earnbyhardik@gmail.com>
2023-08-02 12:15:26 +0530

    Task1 compeleted

---
+++ model/user.js
@@ -0,0 +1,27 @@
+const { ObjectId } = require("mongodb");
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  userName: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+
+  phone: {
+    type: Number,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+module.exports = mongoose.model("Users", userSchema);
